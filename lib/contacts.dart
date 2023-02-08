be6d55d28104aae126683328453a52d62373147c
kayyum0 <sd21if002@gmail.com>
2023-02-08 21:09:23 +0530

    Android

---
+++ lib/contacts.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ContactsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.red,
+        child: OverflowBox(
+          maxHeight: 500,
+          maxWidth: 500,
+          child: Container(
+            color: Colors.white.withOpacity(0.5),
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ),
+      ),
+    );
+  }
+}
