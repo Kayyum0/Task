244a3591a04646edf28ca9cf9dd26f217a5d2549
kayyum0 <sd21if002@gmail.com>
2023-02-12 17:29:21 +0530

    changes

--- lib/events.dart
+++ lib/events.dart
@@ -8,27 +8,14 @@ class EventsPage extends StatefulWidget {
 class _EventsPageState extends State<EventsPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text((''
-              'logo')),
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.asset('assets/images/logo.png'),
-            Container(
-              padding: EdgeInsets.all(15.00),
-              child: Text(
-                ' Petter John',
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+   return Container(
+     child: Row(
+       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+       children: [
+         Image.asset('assets/images/icon.png'),
+         Text('Petter John'),
+       ],
+     ),
+   );
   }
 }
