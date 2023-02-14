a39e160926e0a03b5f4ad392d63bbbc5c4907b92
kayyum0 <sd21if002@gmail.com>
2023-02-14 21:43:06 +0530

    changes

--- lib/events.dart
+++ lib/events.dart
@@ -8,12 +8,12 @@ class EventsPage extends StatefulWidget {
 class _EventsPageState extends State<EventsPage> {
   @override
   Widget build(BuildContext context) {
-   return Container(
+   return Center(
      child: Row(
        mainAxisAlignment: MainAxisAlignment.spaceBetween,
        children: [
-         Image.asset('assets/images/icon.png'),
-         Text('Petter John'),
+        Image.asset('assets/images/logo.png'),
+        Text('Petter John'),
        ],
      ),
    );
